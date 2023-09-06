7847db9e00c0b30263a2b1989f8e91f5d8d94d2f
marcely melo <Marcelyjfmelo@gmail.com>
2023-09-06 16:34:09 -0300

    Att

---
+++ Projeto CT2/Assets/Scripts/HealthController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    [SerializeField] int numLives = 3;
+  
+    void Update()
+    {
+        
+    }
+}
